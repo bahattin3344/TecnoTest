6fbc12370cae0e41d38c3d95a0de7a80bc00a784
deniz <denizan561@gmail.com>
2023-09-01 20:05:06 +0300

    Master Branch ve Base Driver Düzenlemeleri Yapıldı

---
+++ src/Master.java
@@ -0,0 +1,13 @@
+import Utility.BaseDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Master extends BaseDriver {
+    @Test
+    public void Tecno(){
+
+    }
+}
